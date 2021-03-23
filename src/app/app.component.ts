9bf0440d60a0c6e9544c3d415a3bd87fcae3eeb6
Humberto Balleza <serballeza@gmail.com>
2021-03-23 09:18:57 -0600

    Prueba commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { SplashScreenPluginWeb } from '@capacitor/core';
+import { appInitialize } from '@ionic/angular/app-initialize';
+
+ 
+@Component({
+  selector: 'app-root',
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.scss'],
+})
+export class AppComponent {
+  constructor(){
+
+  }
+    
+}
+
